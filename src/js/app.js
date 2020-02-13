d19b674720408cab0b6ab22b431c71290c677d02
widehem1u <martin.widehem7@etu.univ-lorraine.fr>
2020-02-13 16:40:28 +0100

    register le js et l'html

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as login from "./login.js";
+import * as signup from "./signup.js";
 
 $(document).ready(function () {
     login.init();
+    signup.init();
 });
 
---
+++ src/js/signup.js
@@ -0,0 +1,39 @@
+function signup() {
+    $.post(
+        'http://localhost/CrazyCharlyDay/src/inc/signup.inc.php',
+        {
+            prenom : $("#inputFirstName").val(),
+            nom : $("#inputLastName").val(),
+            mail : $("#inputEmailAddress").val(),
+            pwd : $("#inputPassword").val(),
+            pwd_repeat : $("#inputPassword").val(),
+        },
+        returnData,
+
+        'json'
+    );
+}
+
+function returnData(Data){
+    console.log("zgueg");
+    if (Data.sucess === false){
+        console.log(1);
+        switch (Data.error) {
+            case "emptyfields":
+                console.log("empty");
+                break;
+            case "sqlerror":
+                console.log("sqlerror");
+                break;
+            default:
+                console.log("error");
+                break;
+        }
+    }else {
+        document.location.href="http://localhost/CrazyCharlyDay/login.html";
+    }
+}
+export function init() {
+    $("#submitbutton").click(signup);
+    console.log(1);
+}
